639d60f5d3db4673c20a1c99c2aeca3392879783
iremharnak <iremharnak@users.noreply.github.com>
2022-05-09 10:57:45 -0400

    some html on landing page

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   id: Number,
   name: String,
-  
+  email: String,
+  googleId: String,
 })
 
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./config/database');
-
+// load the env vars
+// require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 

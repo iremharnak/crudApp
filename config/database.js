8900e8f17f45743594cf03d05975faa0196621e5
iremharnak <iremharnak@users.noreply.github.com>
2022-05-23 11:21:16 -0400

    all changes made trying to seed mongod
    All attempts failed

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/major', {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-
+  
 });
 
 const db = mongoose.connection;
